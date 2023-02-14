211666ae0da3776bb33abc33f8c62f40bfd3bb38
winterWX <wangxiang081457@163.com>
2023-02-15 01:26:39 +0800

    函数的执行过程

---
+++ js/02.函数调用函数的执行过程/3.3作用域提升案例三.js
@@ -0,0 +1,14 @@
+/**
+ * 案例一
+*/
+
+function boo(){
+    var a = b = 100
+    /**
+     *  var a  = 100 - 局部
+     *  b = 100  - 会添加到全局
+     */
+}
+
+console.log(a)    // undefined
+console.log(b)    // 100
