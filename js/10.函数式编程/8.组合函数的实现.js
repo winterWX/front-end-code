7cc6ddac3294e77678fc7ed0d5c6b33b1b043068
winterWX <wangxiang081457@163.com>
2023-02-22 13:59:40 +0800

    面向对象

--- js/10.函数式编程/8.组合函数的实现.js
+++ js/10.函数式编程/8.组合函数的实现.js
@@ -1,7 +1,3 @@
-
-
-
-
 function add(num){
    return num + 2
 }
@@ -9,5 +5,12 @@ function add(num){
 function square(num){
     return num ** 2
 }
+ 
+function composeComponent(m,n){
+    return function(count){
+        return n(m(count))
+    }
+}
 
-
+var fn = composeComponent(add,square)  //先加后平方
+fn(10)
