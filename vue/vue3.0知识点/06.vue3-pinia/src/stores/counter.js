d9475ff1ce84a9f9d307d5b4eec1a795a17575d2
winterWX <wangxiang081457@163.com>
2023-03-27 23:28:28 +0800

    code update

---
+++ vue/vue3.0知识点/06.vue3-pinia/src/stores/counter.js
@@ -0,0 +1,27 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+// 组合函数的方法
+export const useCounterStore = defineStore('counter', () => {
+  // ref 就是 state
+  const count = ref(0)
+  // computed 就是 getter
+  const doubleCount = computed(() => count.value * 2)
+  // function 就是action
+  function increment() {
+    count.value++
+    console.log('count.value===',count.value)
+  }
+
+  return { count, doubleCount, increment }
+
+  /**
+   * setup store 相比 option store 
+   *  在 setup store 
+   *   1.里可以使用 watch
+   *   2.组合更加自用
+   *   3.state其实就是reactive包装的一个对象
+   */
+})
+
+
---
+++ vue/vue3.0知识点/06.vue3-pinia/src/views/contentView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>映射读取pinia里的值</h2>
+        <div>读取pinia里的值{{contentStore.nameCode}} - {{contentStore.age}}</div>
+        <button @click="changeState()">点击读取</button>
+    </div>
+</template>
+<script lang="ts" setup>
+import { useContentStore  } from '../stores/content.js'
+const contentStore = useContentStore()
+console.log('contentStore===',contentStore.nameCode)
+const changeState = ()=>{
+    contentStore.$patch({
+        nameCode: 'change name',
+        age: 3000
+    })
+}
+
+
+</script>
