725565638f1fd9faaa7ebc41e47089e4fe73d8bb
winterWX <wangxiang081457@163.com>
2023-05-28 22:40:12 +0800

    code update

--- ES语法系列/es12/1._finalizationRegistry的用法.js
+++ ES语法系列/es12/1._finalizationRegistry的用法.js
@@ -3,16 +3,16 @@
  *   
  *   1. finalizationRegistry 是一个类
  *   当对象被销毁了，就会进行监听
- *  
+ *   
  */
 
 
-const finalizationRegistry = new FinalizationRegistry((val)=>{
-    console.log('监听的对象被销毁了',val)
+const finalizationRegistry = new FinalizationRegistry((val) => {
+    console.log('监听的对象被销毁了', val)
 })
 
 let obj = {
-    name:'liu liu'
+    name: 'liu liu'
 }
 
 finalizationRegistry.register(obj, 'obj')
---
+++ websocket/index.mm.md
@@ -0,0 +1,15 @@
+## websocket
+ - 出现
+    - 22222
+    - 22222
+ - 用法
+
+
+
+ I do what I do
+
+ - [ https://www.baidu.com ]
+
+
+
+
